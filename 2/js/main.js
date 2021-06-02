c7df2f7a0ac07660df64a8ec11a045b06bffa460
Keks <keks@htmlacademy.ru>
2021-06-02 21:02:17 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -2,17 +2,19 @@
 
 let getRandomNumber = function  (min, max) {
   if (min >= max) {
-    return"Не правильно задан параметр"
+    throw new Error
   } else if (min >= 0 && max > 0) {
     return Math.round(Math.random() * (max - min)) + min;
   }
+
+  throw new Error
 }
 
 
 // Функция проверяющия длину строчки
 
 let checkLength = function (line , maxLength) {
-  if (line > maxLength) {
+  if (line.length > maxLength) {
     return false
   }
 
