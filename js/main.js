05fc4cef3093d1035feae81da9225ebad09a30ec
Евгений <glazov_evgen@mail.ru>
2021-06-02 20:59:55 +0300

    комит

--- js/main.js
+++ js/main.js
@@ -2,30 +2,19 @@
 
 let getRandomNumber = function  (min, max) {
   if (min >= max) {
-<<<<<<< HEAD
     throw new Error
   } else if (min >= 0 && max > 0) {
     return Math.round(Math.random() * (max - min)) + min;
   }
 
   throw new Error
-=======
-    return"Не правильно задан параметр"
-  } else if (min >= 0 && max > 0) {
-    return Math.round(Math.random() * (max - min)) + min;
-  }
->>>>>>> 823a21cb6d916f62164d4aa27502f4ee38012ce1
 }
 
 
 // Функция проверяющия длину строчки
 
 let checkLength = function (line , maxLength) {
-<<<<<<< HEAD
   if (line.length > maxLength) {
-=======
-  if (line > maxLength) {
->>>>>>> 823a21cb6d916f62164d4aa27502f4ee38012ce1
     return false
   }
 
